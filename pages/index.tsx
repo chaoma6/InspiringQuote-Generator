c1a15099d65946cdcd049acb9e4c98a7afeced31
Chao Ma <machaosydney@gmail.com>
2023-07-05 18:24:34 +1000

    [Initial commit] Project setup

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inspirational Quote Generator</title>
+        <meta name='description' content='Inspirational Quote Generator' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={styles.main}>123</main>
+    </>
+  );
+}
